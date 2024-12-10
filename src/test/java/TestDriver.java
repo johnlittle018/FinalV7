8ce786e0b5e35672e775f0a09c36848fd94d8f59
johnlittle018 <jl668124@hotmail.com>
2024-12-10 16:30:48 -0500

    testing test

--- src/test/java/TestDriver.java
+++ src/test/java/TestDriver.java
@@ -1,13 +1,11 @@
 import org.junit.Test;
 import org.junit.runners.JUnit4;
 
-@runwith(JUnit4.class)
 public class TestDriver {
 
-    @Test
-    public void testCar(){
-        Assert.assertEquals(5, 5);
-    }
+
+    Assert.assertEquals(5, 5);
+
 
 
 
