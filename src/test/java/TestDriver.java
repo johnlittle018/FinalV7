00196a0aabe25466f02e1738f33c40cdb5652b9e
johnlittle018 <jl668124@hotmail.com>
2024-12-10 16:20:01 -0500

    AddedTestFile

---
+++ src/test/java/TestDriver.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
+@runwith(JUnit4.class)
+public class TestDriver {
+
+    @Test
+    public void testCar(){
+        Assert.assertEquals(5, 5);
+    }
+
+
+
+
+
+
+}
